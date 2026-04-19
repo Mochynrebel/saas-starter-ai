44c24dd38d72433bac7aa042d5ca20ec42828da5
Mochynrebel <840666510@qq.com>
2026-04-19 19:54:17 +0800

    google

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,26 +5,20 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-  const gaId = process.env.NEXT_PUBLIC_GA_ID;
   return (
     <html suppressHydrationWarning>
       <head>
-        {gaId && (
-          <>
-            {/* Google Analytics */}
-            <script async src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}></script>
-            <script
-              dangerouslySetInnerHTML={{
-                __html: `
-                  window.dataLayer = window.dataLayer || [];
-                  function gtag(){dataLayer.push(arguments);}
-                  gtag('js', new Date());
-                  gtag('config', '${gaId}', { page_path: window.location.pathname });
-                `,
-              }}
-            />
-          </>
-        )}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-CTES637PN1"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-CTES637PN1');
+            `,
+          }}
+        />
       </head>
       <body>
         {children}
